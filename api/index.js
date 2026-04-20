7d17dcd45f5355adabc9bb0441d04c04fa34b7b1
sandybedi <sandy29.bedi@gmail.com>
2026-04-20 11:33:46 +0530

    Use @vercel/node with includeFiles to correctly bundle and serve frontend dist

--- api/index.js
+++ api/index.js
@@ -1,11 +1,2 @@
 const app = require('../backend/server');
-
-module.exports = (req, res) => {
-  // Temp debug: expose req.url so we can see what Vercel passes in
-  if (req.url === '/api/debug' || req.url.endsWith('?debug')) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ reqUrl: req.url, method: req.method }));
-    return;
-  }
-  return app(req, res);
-};
+module.exports = (req, res) => app(req, res);
